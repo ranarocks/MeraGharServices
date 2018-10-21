747ad17c38fdc4ffdb51970c1835a707c729dab8
ranarocks <jatinrana1988@gmail.com>
2018-10-21 09:51:38 +0530

    feedback services

---
+++ MeraGhar.DAL/FeedBackDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using MeraGhar.Models;
+using SQLDataUtility;
+using HelperExtensions;
+
+namespace MeraGhar.DAL
+{
+   public class FeedBackDAL
+    {
+        public int ModifyFeedBack(FeedBackModel model)
+        {
+            SqlParameter[] param =
+            {
+                new SqlParameter("@Type", model.Type),
+                new SqlParameter("@Name", model.Name),
+                new SqlParameter("@EmailAddress", model.EmailAddress),
+                new SqlParameter("@PhoneNumber", model.PhoneNumber),
+                new SqlParameter("@Message", model.Message)
+            };
+            return Convert.ToInt16(SqlHelper.ExecuteScalar(System.Data.CommandType.StoredProcedure, "USP_FeedbackContact", param));
+        }
+    }
+}
---
+++ MeraGhar.Utilities/ServiceResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeraGhar.Utilities
+{
+ 
+        public class ServiceResponse<T>
+        {
+            public int StatusCode { get; set; }
+            public List<string> Messages { get; set; }
+            public IList<T> List { get; set; }
+            public T Obj { get; set; }
+            public ServiceResponse()
+            {
+                List = new List<T>();
+                Messages = new List<string>();
+            }
+        }
+    
+}
